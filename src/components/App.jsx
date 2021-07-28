fbef8a9a75a0a7c481a00faa3d33e3a2e6e11e12
VatsalyaGoswami <vatsalya559@gmail.com>
2021-07-29 02:43:54 +0530

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+
+
+function App() {
+  const getLocalItems = () => {
+    let list = localStorage.getItem('lists');
+    if (list) {
+      return JSON.parse(localStorage.getItem('lists'));
+
+    }
+    else {
+      return [];
+    }
+  }
+  const [notes, setNotes] = useState(getLocalItems());
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+  // add data to local storage
+  useEffect(() => {
+    localStorage.setItem('lists', JSON.stringify(notes))
+
+  }, [notes])
+
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
